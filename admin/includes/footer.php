62207006da0944000d2e9202e02bf70eb13fbac8
ShantanuD20 <120300843+ShantanuD20@users.noreply.github.com>
2022-12-19 17:07:34 +0600

    None

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+     <strong>Copyright &copy; 2020 <a href="https://www.facebook.com/">Shopping Cart</a></strong>
+</footer>
